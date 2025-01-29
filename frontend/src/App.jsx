301d2e6329e4baf502f78c82a7524cf4225ef9ca
invcble <imonbera13@gmail.com>
2025-01-29 16:00:45 -0500

    Updated first build for hello world

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message || "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
